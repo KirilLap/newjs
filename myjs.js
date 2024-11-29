a56fe8d48eeefa0591c9a1fbc004be4348d00ab6
Kirill <example@gmail.com>
2024-11-29 16:12:18 +0500

    first commit

---
+++ myjs.js
@@ -0,0 +1,53 @@
+/*var one = 'Hello';
+var movie = "The Matrix";
+
+//alert(one);
+//console.log(one);
+
+//document.getElementById('message').innerHTML = 'Hello';
+//document.getElementById('movie').innerHTML = movie;
+
+$('#test').text(one);*/
+
+
+
+/*var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;*/
+
+var number = 5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log("string"*9);
+
+let something;
+console.log(something);
+
+let person = {
+    name: "John",
+    age: 25,
+    IsMarried: false
+};
+
+
+console.log(person.name);
+console.log(person["age"]);
+
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+console.log(arr[1]);
+
+//alert("Hello World");
+/*let answer = confirm("Are you here?");
+
+console.log(answer);*/
+
+let answer = prompt("Есть ли вам 18 лет?", "Да");
+
+console.log(typeof(answer));
